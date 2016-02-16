43bdad13c608e065854a31f4b32c663a078038d2
kasun <kkariyawasam@isa.ae>
2016-02-16 20:24:39 +0530

    commit

---
+++ src/main/java/com/isa/aroxHelper/AroxHelper.java
@@ -0,0 +1,16 @@
+package com.isa.aroxHelper;
+
+/**
+ * Created by kasun on 2/16/16.
+ */
+public class AroxHelper {
+
+
+
+    public static void main(String args []) {
+
+        System.out.println("Test By Kasun Kariyawasam");
+    }
+
+
+}
---
+++ src/main/java/com/isa/couchbaseDao/CouchBaseDao.java
@@ -0,0 +1,11 @@
+package com.isa.couchbaseDao;
+
+
+/**
+ * Created by kasun on 2/16/16.
+ */
+public class CouchBaseDao {
+//
+//    Cluster cluster = CouchbaseCluster.create();
+//    Bucket bucket = cluster.openBucket();
+}
